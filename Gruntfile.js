99da5eba2786d08e048a3856bd34e75b2394e100
rob-datarati <rob.thwaites@datarati.com.au>
2015-08-05 22:06:52 +1000

    Baseline function wokring and improved. TODO:  needs some extra finesse

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
               server: {
                 baseDir: './'   
               }
-//              startPath: "/mcguigan/",
+//              startPath: "/folder/",
 //              browser: "safari"
           }
         },
