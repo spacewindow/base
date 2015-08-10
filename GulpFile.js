46b7e566c67261d6ffce0917c8291fe14a6117d6
rob-datarati <rob.thwaites@datarati.com.au>
2015-08-11 09:48:02 +1000

    $basline variable wokring well. TODO: typography tweaks on buttons / tables / forms / menus

---
+++ GulpFile.js
@@ -0,0 +1,106 @@
+'use strict';
+
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var srcmaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+//gulp.task('prod', ['sass-prod', 'prefix']);
+gulp.task('start', ['browsersync']);
+
+// Gulp browsersync task
+gulp.task('browsersync', ['sass'], function() {
+
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Sass compiler
+gulp.task('sass', function () {
+
+    gulp.src('scss/styles.scss')
+
+        //  Stops crashing watch on error.
+        .pipe(plumber())
+
+        //  Initialise sourcemaps.
+        .pipe(srcmaps.init())
+
+        //  Compile Sass files, log errors.
+        .pipe(sass({
+            outputStyle: 'nested'
+        }).on('error', sass.logError))
+
+        //  Write the sourcemaps to the current directory.
+        .pipe(srcmaps.write('/'))
+
+        //  Set the output destination for the final file.
+        .pipe(gulp.dest('css'))
+
+        // Sync with browsersync
+        .pipe(browserSync.stream());
+
+});
+
+gulp.task('uglify', function () {
+    gulp.src(['private/js/docCookies.js', 'private/js/app.js'])
+
+        //  Stops crashing watch on error.
+        .pipe(plumber())
+
+        //  Initialise sourcemaps.
+        .pipe(srcmaps.init())
+
+        //  Concat all files.
+        .pipe(concat('datarati-lc-tmca.js'))
+
+
+        //  Set the destination folder for the concat file.
+//        .pipe(gulp.dest('public/js'))
+
+        //  Uglify, with settings.
+        .pipe(uglify({
+            mangle: true,
+            warnings: true
+        }))
+
+        //  Rename the output.
+        .pipe(rename({
+            extname: '.min.js'
+        }))
+
+        //  Write the sourcemaps to the current directory.
+        .pipe(srcmaps.write('/'))
+
+        //  Set the output destination for the final file.
+        .pipe(gulp.dest('public/js'));
+
+});
+
+// Prefix CSS
+gulp.task('prefix', function () {
+    gulp.src('css/styles.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('css/styles.css'));
+});
+
+gulp.task('watch', function () {
+
+    gulp.watch('scss/*.scss', ['sass']);
+    //gulp.watch(['private/js/docCookies.js', 'private/js/app.js'], ['uglify']);
+
+});
